8bd8456a8eb99529f9a65fb9a866d84c67733053
belalelmi <belalelmi@gmail.com>
2021-11-03 20:34:41 -0400

    initial commit

--- client.js
+++ client.js
@@ -3,4 +3,28 @@ const net = require('net');
 const host = '165.227.47.243'
 const port = 50541
 
-module.exports = { host, port }
+const connect = () => {
+  const name = 'Name: BHE';
+  const conn = net.createConnection({
+    host,
+    port,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.write(`${name}`)
+
+  conn.on('connect', () => {
+    conn.write('Connected!');
+  })
+
+  conn.on('data', (data) => {
+    console.log("Incoming: " + data);
+    // console.log(data);
+
+  });
+  return conn;
+
+};
+
+module.exports = connect;
---
+++ input.js
@@ -0,0 +1,32 @@
+// const { connect } = require('./client');
+const stdin = process.stdin;
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = (input) => {
+  if (input === '\u0003') {
+    process.exit();
+  } else if (input === 'w') {
+    connection.write("Move: up");
+  } else if (input === 'd') {
+    connection.write("Move: right");
+  } else if (input === 's') {
+    connection.write("Move: down");
+  } else if (input === 'a') {
+    connection.write("Move: left");
+  }
+
+};
+
+
+
+module.exports = setupInput;
